17cf8467e2ae13e27ce553d2744947d213a76d73
Kyle Snowden <39664771+katteranga@users.noreply.github.com>
2024-06-28 10:33:15 -0700

    Add files via upload

---
+++ linux_setup.sh
@@ -0,0 +1,18 @@
+# Linux setup
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install vim synaptic git unzip curl ffmpeg ubuntu-restricted-extras guvcview fonts-firacode
+sudo apt install timeshift google-chrome-stable steam 
+
+# Online accounts -> google chrome
+
+# Install discord
+
+# Settings -> OS update and recovery -> Update Recovery Partition
+
+# Settings -> Firmware -> Install updates if available
+Settings -> Date and time -> Enable automatic date and time
+Settings -> Displays -> Night Light tab -> enable
+Settings -> Displays tab -> enable fractional scaling -> Switch to the most suitable scaling mode
+Settings -> Desktop -> Dock
+
